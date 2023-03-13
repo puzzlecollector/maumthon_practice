4fccfa020e767829aac0e7de1a50237807db6348
Luke Kim <puzzlecollector1123581321@gmail.com>
2023-03-13 14:06:38 +0900

    Create gsearch.js

---
+++ gsearch.js
@@ -0,0 +1,41 @@
+const request = require("request")
+
+var URL = "https://serpapi.com/search?engine=google&q=%22BERT&api_key=f21f326b550a02e9512eb47d920a58bfebd441f41cf2b4c558b620a185ebfa7e";
+//var URL = "https://serpapi.com/search?engine=google_scholar&q=%22BERT&api_key=f21f326b550a02e9512eb47d920a58bfebd441f41cf2b4c558b620a185ebfa7e";
+
+
+const options = {
+  url: URL,
+  type: "get",
+};
+
+request.get(options, function(err,data){
+  if (err){
+    console.log(err);
+  }else{
+    var search_data = data.body;
+    var object_data = JSON.parse(search_data);
+    var related_questions = object_data["related_questions"];
+    var q_str = ""
+    for (var i = 0; i < related_questions.length; i++){
+      q_str += related_questions[i]["question"] + " " + related_questions[i]["snippet"] + "\n\n"
+    }
+    console.log(q_str)
+  }
+})
+
+
+//question, snippet
+
+/*
+var reviews = ip1.product_results.user_reviews
+
+var buff_list = ""
+for (var i=0; i < reviews.length; i++){
+        buff_list += reviews[i]["text"] + "\n\n"
+}
+
+var result = `이 제품에 리뷰들이 주어졌을 때, 하나의 리뷰로 요약하고 한글로 번역해줘 $(buff_list)`;
+
+return {"text": result};
+*/
